7921b7612838bbff8c1692b52f0c4b9375c64852
chr15 <chr15@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-20 13:07:56 +0000

    Updated Core Library to 2.0.6
    git-svn-id: https://plugins.svn.wordpress.org/summy@700746 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.0.3/tags/1.0.1/lib/Summy/Score/Position.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * @package		Summy
+ * @version		$Id: Position.php 93 2013-02-04 18:26:25Z Tefra $
+ * @author		Christodoulos Tsoulloftas
+ * @copyright	Copyright 2011-2013, http://www.komposta.net
+ */
+namespace Summy\Score;
+
+class Position
+{
+
+	/**
+	 * Shortcut method kinda...
+	 * @param string $method
+	 * @param integer $totalParagraphs
+	 * @param integer $sentencesInParagraph
+	 * @param integer $paragraph
+	 * @param integer $sentence
+	 * @return float
+	 */
+	public function score($method, $totalParagraphs, $sentencesInParagraph, $paragraph, $sentence)
+	{
+		if(method_exists($this, $method))
+		{
+			return $this->{$method}($totalParagraphs, $sentencesInParagraph, $paragraph, $sentence);
+		}
+	}
+
+	/**
+	 * Return the Position Weight for a sentence based on the Baxendale investigation
+	 *
+	 * Baxendale (1958) investigated a sample of 200 paragraphs to determine
+	 * where the important words are most likely to be found. He concluded that
+	 * in 85% of the paragraphs,  the first sentence was a topic sentence and in
+	 * 7% of the paragraphs, the final one.
+	 *
+	 * @param integer $totalParagraphs
+	 * @param integer $sentencesInParagraph
+	 * @param integer $paragraph
+	 * @param integer $sentence
+	 * @return float
+	 */
+	public function baxendale($totalParagraphs, $sentencesInParagraph, $paragraph, $sentence)
+	{
+		switch(true)
+		{
+			case ($sentence == 1):
+			case ($sentence == $sentencesInParagraph):
+				return 0.5;
+				break;
+			default:
+				return 0;
+				break;
+		}
+	}
+
+	/**
+	 * Return the Position Weight for a sentence based on the hypothesis that
+	 * first paragraphs/sentences are the most meaningfull to a document, which
+	 * applies to small articles, like newspaper news.
+	 *
+	 * @param integer $totalParagraphs
+	 * @param integer $sentencesInParagraph
+	 * @param integer $paragraph
+	 * @param integer $sentence
+	 * @return float
+	 */
+	public function news($totalParagraphs, $sentencesInParagraph, $paragraph, $sentence)
+	{
+		return (($totalParagraphs - $paragraph + 1) / $totalParagraphs) * (($sentencesInParagraph - $sentence + 1) / $sentencesInParagraph);
+	}
+}
+
+?>
---
+++ tags/1.0.3/trunk/summy.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+  Plugin Name: Summy: Excerpt Extraction
+  Plugin URI: http://www.komposta.net/article/wp-summy
+  Description: Summy can generate excerpts for your posts by applying various algorithms for automatic summarization extraction.
+  Version: 1.0.3
+  Author: Christodoulos Tsoulloftas
+  Author URI: http://www.komposta.net
+  ---------------------------------------------------------------------------------
+  Copyright 2013  Christodoulos Tsoulloftas  (http://www.komposta.net/core/contact)
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+defined('ABSPATH') || die('Nothing Here...');
+
+add_action('add_meta_boxes', function() {
+	require_once dirname(__FILE__) . '/inc/ui.php';
+});
+
+add_action('wp_ajax_summy', function() {
+	require_once dirname(__FILE__) . '/inc/backend.php';
+});
+
+register_deactivation_hook(__FILE__, function() {
+	require_once dirname(__FILE__) . '/inc/setup.php';
+	summy_deactivate();
+});
+
+register_activation_hook(__FILE__, function() {
+	require_once dirname(__FILE__) . '/inc/setup.php';
+	summy_activate();
+});
+
+?>
